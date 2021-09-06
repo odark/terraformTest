6ceef2dbc8f0e63fd52b4e16facf462714a0d769
오승현 <odark74@gmail.com>
2021-09-06 21:46:24 +0900

    terrform init 1

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  access_key = "AKIA24HTW6KSS725XS3S"
+  secret_key = "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"
+  region = "ap-northeast-2"
+}
+
+resource "aws_instance" "web" {
+  ami = "ami-0ba5cd124d7a79612"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "demo-instance"
+  }
+}
