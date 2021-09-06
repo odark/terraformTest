abdfeeb0a257552482a15fd3a79d651517bdb324
오승현 <odark74@gmail.com>
2021-09-06 22:54:20 +0900

    test3

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 provider "aws" {
   access_key = "AKIA24HTW6KSS725XS3S"
   secret_key = "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"
@@ -14,8 +5,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami = "ami-0ba5cd124d7a79612"
+  ami           = "ami-0ba5cd124d7a79612"
   instance_type = "t2.micro"
+
   tags = {
     Name = "demo-branch"
   }
