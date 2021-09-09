3faed88a181d1019930527d2ca4f854981638eda
오승현 <odark74@gmail.com>
2021-09-09 16:15:25 +0900

    text update3

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "ec2_name" {
-    value = "${aws_instance.web.name}"
+    value = "${aws_instance.web.tags.name}"
 }
 
 output "ec2_id" {
-    value = "${aws_inatance.web.id}"
+    value = "${aws_instance.web.id}"
 }
