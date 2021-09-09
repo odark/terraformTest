daf25ca9146077f629316b1700be7a5eaba8178c
오승현 <odark74@gmail.com>
2021-09-09 10:25:47 +0900

    hidden secret

--- variables.tf
+++ variables.tf
@@ -3,12 +3,3 @@ variable "region" {
   description = "AWS region"
   default = "ap-northeast-2"
 }
-variable "access_key_id" {
-  type = string
-  description = "aws access key id"
-}
-
-variable "security_access_key" {
-  type = string
-  description = "aws security key"
-}
